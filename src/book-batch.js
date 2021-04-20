81f251f82dcaa486cfd18ba05b81216b3cad72a5
sandeepmehta2155 <66156650+sandeepmehta2155@users.noreply.github.com>
2021-04-21 02:37:05 +0530

    added batches from backend

--- src/book-batch.js
+++ src/book-batch.js
@@ -1,36 +1,22 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export function BookBatches() {
+  const [img, setImg] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://e-commerce.sandeepmehta215.repl.co/bookbatches")
+      .then((resp) => {
+        if (resp.status === 200) setImg(resp.data.images);
+        else setImg("loading...");
+      });
+  }, []);
+
   return (
     <div className="bookBatches">
-      <img
-        className="bookBatchPills"
-        src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Books/092020/Children-Book-Shelf_PC.jpg"
-        alt="loading"
-      />
-      <img
-        className="bookBatchPills"
-        src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Books/102020/Examcentral/Exam-Central-PC.jpg"
-        alt="loading"
-      />
-      <img
-        className="bookBatchPills"
-        src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Books/032020/editors-corner-PC-final.jpg"
-        alt="loading"
-      />
-      <img
-        className="bookBatchPills"
-        src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Books/112020/Offerzone-pc.jpg"
-        alt="loading"
-      />
-      <img
-        className="bookBatchPills"
-        src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Books/112020/HIGHER-EDUCATION.jpg"
-        alt="loading"
-      />
-      <img
-        className="bookBatchPills"
-        src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Books/112020/SCHOOL.jpg"
-        alt="loading"
-      />
+      {img.map((obj) => (
+        <img className="bookBatchPills" src={obj} alt="loading" />
+      ))}
     </div>
   );
 }
