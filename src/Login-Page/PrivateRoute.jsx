82d09f0b4108ca848be1f447e69b93485c970d5e
sandeepmehta2155 <66156650+sandeepmehta2155@users.noreply.github.com>
2021-04-30 02:31:41 +0530

    login backend auth done

--- src/Login-Page/PrivateRoute.jsx
+++ src/Login-Page/PrivateRoute.jsx
@@ -5,6 +5,9 @@ export function PrivateRoute() {
   const [usersFromDataBase, setUsersFromDataBase] = useState("");
   const [userInput, setUserInput] = useState("");
   const [passwordInput, setUserPassword] = useState("");
+  const [userExists, setUserExists] = useState("no");
+  const [loginStatus, setloginStatus] = useState("no");
+
   useEffect(() => {
     axios
       .get("https://e-commerce.sandeepmehta215.repl.co/login")
@@ -12,12 +15,28 @@ export function PrivateRoute() {
   }, []);
 
   function CheckAuth() {
-    usersFromDataBase.find(({ username }) =>
-      username === userInput ? console.log("yes") : console.log("no")
-    );
-    return <div> </div>;
-  }
+    if (usersFromDataBase)
+      usersFromDataBase.find(({ username, password }) => {
+        switch (username) {
+          case userInput:
+            setUserExists("none");
+            break;
+          default:
+            break;
+        }
+        switch (password) {
+          case passwordInput:
+            setloginStatus("yes");
+            break;
+          default:
+            break;
+        }
+      });
 
+    console.log(loginStatus, userExists);
+
+    userExists === "none" ? setUserExists("none") : setUserExists("block");
+  }
   return (
     <div className="modal1">
       <h2 style={{ color: "coral" }}>Subscribe</h2>
@@ -31,6 +50,11 @@ export function PrivateRoute() {
           onChange={(e) => setUserInput(e.target.value)}
         />
         <br />
+        {userInput && (
+          <small style={{ color: "red", display: userExists }}>
+            User doesn't exists
+          </small>
+        )}
         <br />
         <label>Enter your password : </label>
         <input
@@ -41,7 +65,12 @@ export function PrivateRoute() {
         />
       </body>
       <br />
-      <button className="btn btn-primary" onClick={() => CheckAuth()}>
+      <button
+        className="btn btn-primary"
+        onClick={() => {
+          CheckAuth();
+        }}
+      >
         Subscribe
       </button>
     </div>
