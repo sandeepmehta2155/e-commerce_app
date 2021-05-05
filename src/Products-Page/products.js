55ef5f02a5fc80417c04bee79a2371644f1e538b
sandeepmehta2155 <66156650+sandeepmehta2155@users.noreply.github.com>
2021-05-05 11:04:37 +0530

    filters added in product page

--- src/Products-Page/products.js
+++ src/Products-Page/products.js
@@ -1,13 +1,17 @@
 import { useFilter } from "./filter-context";
 import { useProd } from "./product-context";
 import Select from "react-select";
+import { useState } from "react";
 
 export const Products = () => {
-  const { itemsInProduct, productDispatch } = useProd();
-  const { IncludeOutOfStock } = useFilter();
-  const { fastDelvry } = useFilter();
-  const { value } = useFilter();
+  const [filterModal, setFilterModal] = useState("none");
+  const [sortModal, setSortModal] = useState("none");
+  const { IncludeOutOfStock, setIncludeOutOfStock } = useFilter();
+  const { fastDelvry, setFastDelivery } = useFilter();
+  const { value, setValue } = useFilter();
   const { sorting, setSorting } = useFilter();
+
+  const { itemsInProduct, productDispatch } = useProd();
 
   const options = [
     { value: "HighToLow", label: "Price : High to Low" },
@@ -18,8 +22,6 @@ export const Products = () => {
     <>
       <h1 className="productsHead">Books in focus</h1>
       <br />
-
-      {/* <input type="search" name="sort" placeholder="🔍   Search" /> */}
       <span className="containerForSelectComponent">
         <label for="sortingSelectBoxs">Sort by : </label>
         <Select
@@ -153,6 +155,93 @@ export const Products = () => {
             );
           })}
       </ul>
+
+      <div className="filterBox">
+        <fieldset className="filterFooter" style={{ display: filterModal }}>
+          <legend>Filters</legend>
+          <input
+            type="checkbox"
+            name="checkOne"
+            value="IncludeOutOfStock"
+            onChange={() => setIncludeOutOfStock(!IncludeOutOfStock)}
+          />
+          <label htmlFor="checkOne">Include out of stock </label>
+
+          <input
+            type="checkbox"
+            name="checkTwo"
+            value="fastDelivery"
+            onChange={() => setFastDelivery(!fastDelvry)}
+          />
+          <label htmlFor="checkTwo">Fast Delivery</label>
+          <br />
+          <br />
+          <input
+            type="range"
+            min="1"
+            max="1000"
+            className="slider"
+            value={value}
+            onChange={({ target: { value: radius } }) => {
+              setValue(radius);
+            }}
+          />
+          <div>Show items greater than : {value} Rs</div>
+        </fieldset>
+        <fieldset className="sortFooter" style={{ display: sortModal }}>
+          <legend>Sort By</legend>
+          <input
+            type="radio"
+            name="sorting"
+            value="lowToHigh"
+            onChange={() => setSorting("lowToHigh")}
+          />
+          <label htmlFor="lowToHigh">High-To-Low</label>
+          <input
+            type="radio"
+            name="sorting"
+            value="highToLow"
+            onChange={() => setSorting("highToLow")}
+          />
+          <label htmlFor="HighToLow">Low-To-High</label>
+        </fieldset>
+      </div>
+      <div className="filterAndSort">
+        <button
+          className="bi bi-sort-down"
+          onClick={() =>
+            sortModal === "none" ? setSortModal("block") : setSortModal("none")
+          }
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="white"
+            viewBox="0 0 16 16"
+          >
+            <path d="M3.5 2.5a.5.5 0 0 0-1 0v8.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L3.5 11.293V2.5zm3.5 1a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1h-1z" />
+          </svg>
+        </button>
+        <button
+          className="bi bi-filter"
+          onClick={() =>
+            filterModal === "none"
+              ? setFilterModal("block")
+              : setFilterModal("none")
+          }
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="white"
+            viewBox="0 0 16 16"
+          >
+            <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2h-11z" />
+          </svg>
+        </button>
+      </div>
     </>
   );
 };
